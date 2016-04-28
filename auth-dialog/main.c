NetworkManager/NetworkManager-fortisslvpn

service: port to libnm

auth-dialog/main.c
@@ -37,7 +37,7 @@
 #include <nm-vpn-plugin-utils.h>
 #include <nm-vpn-password-dialog.h>
 
-#include "src/nm-fortisslvpn-service.h"
+#include "src/nm-fortisslvpn-service-defines.h"
 
 #define KEYRING_UUID_TAG "connection-uuid"
 #define KEYRING_SN_TAG "setting-name"

NetworkManager/network-manager-sstp

auth-dialog/main.c
@@ -38,7 +38,7 @@
 #include <nm-vpn-plugin-utils.h>
 #include <nm-vpn-password-dialog.h>
 
-#include "src/nm-sstp-service.h"
+#include "src/nm-sstp-service-defines.h"
 
 #define KEYRING_UUID_TAG "connection-uuid"
 #define KEYRING_SN_TAG "setting-name"